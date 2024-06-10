c8e3e628cbca485d33ed71ee126ac0982070950e
Vinicius Grossi <viniciussggrossi@gmail.com>
2024-06-10 17:14:20 -0300

    Acréscimo de duas funcionalidades. Separar arquivo por ano e por categoria

--- OCR_Renamer_File.py
+++ OCR_Renamer_File.py
@@ -8,16 +8,34 @@ import zipfile
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 from PIL import Image
+from datetime import datetime
 
 # Configurar o caminho do Tesseract OCR
 caminho_tesseract = r"C:\Users\User\AppData\Local\Programs\Tesseract-OCR"
 pytesseract.pytesseract.tesseract_cmd = os.path.join(caminho_tesseract, 'tesseract.exe')
 
-# Função para renomear os arquivos e retornar um dicionário com os novos nomes
+# Função para extrair o ano da data do texto
+def extract_year_from_text(text):
+    padrao_data = re.compile(r'\b\d{2}/\d{2}/\d{4}\b')
+    datas_encontradas = padrao_data.findall(text)
+    if datas_encontradas:
+        data_extraida = datas_encontradas[0]
+        ano = datetime.strptime(data_extraida, "%d/%m/%Y").year
+        return ano
+    return None
+
+# Função para determinar a categoria (EPI ou Ferramentas) com base no texto
+def categorize_file(text):
+    if re.search(r'\b(EPI|UNIFORMES)\b', text, re.IGNORECASE):
+        return 'EPI'
+    return 'Ferramentas'
+
+# Função para renomear os arquivos e retornar um dicionário com os novos nomes, anos e categorias
 def rename_files_and_get_names(directory, progress_bar, progress_text):
     padrao_nome = re.compile(r'\b(?:[A-Z][A-Z]*\s*){2,}\b')
     contador_nomes = {}
     novo_nomes = {}
+    arquivos_por_ano_e_categoria = {}
 
     arquivos = [f for f in os.listdir(directory) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
     total_arquivos = len(arquivos)
@@ -26,7 +44,10 @@ def rename_files_and_get_names(directory, progress_bar, progress_text):
         caminho_imagem_original = os.path.join(directory, nome_arquivo)
         imagem_original = cv2.imread(caminho_imagem_original)
         texto_completo = pytesseract.image_to_string(imagem_original, lang="por")
+        
         nomes_encontrados = padrao_nome.findall(texto_completo)
+        ano = extract_year_from_text(texto_completo)
+        categoria = categorize_file(texto_completo)
         
         if nomes_encontrados:
             nome_extraido = nomes_encontrados[0][:30]
@@ -43,12 +64,17 @@ def rename_files_and_get_names(directory, progress_bar, progress_text):
             novo_caminho_imagem_original = os.path.join(directory, novo_nome_arquivo)
             os.rename(caminho_imagem_original, novo_caminho_imagem_original)
             novo_nomes[nome_arquivo] = novo_nome_arquivo
+            
+            if ano:
+                if ano not in arquivos_por_ano_e_categoria:
+                    arquivos_por_ano_e_categoria[ano] = {'EPI': [], 'Ferramentas': []}
+                arquivos_por_ano_e_categoria[ano][categoria].append(novo_nome_arquivo)
 
         # Atualizar barra de progresso
         progress_bar.progress((i + 1) / total_arquivos)
         progress_text.text(f"Processing file {i + 1} of {total_arquivos}")
 
-    return novo_nomes
+    return novo_nomes, arquivos_por_ano_e_categoria
 
 # Função para converter PNG para PDF em modo paisagem
 def convert_png_to_pdf(png_path, pdf_path):
@@ -57,7 +83,7 @@ def convert_png_to_pdf(png_path, pdf_path):
     aspect_ratio = width / height
 
     # Definir o tamanho da página do PDF com base na proporção da imagem
-    if aspect_ratio > 1:  # Paisagem
+    if (aspect_ratio > 1):  # Paisagem
         pdf_width = 792  # 11 polegadas em pontos (1 polegada = 72 pontos)
         pdf_height = pdf_width / aspect_ratio
     else:  # Retrato ou quadrado
@@ -76,7 +102,7 @@ def main():
     convert_to_pdf = st.checkbox("Convert to PDF")
     output_zip_name = st.selectbox(
         "Select the name for the output ZIP file",
-        ["LOTE 01 EPI", "LOTE 01 FERRAMENTAS", "LOTE 03 EPI", "LOTE 03 FERRAMENTAS", "LOTE 05 EPI", "LOTE 05 FERRAMENTAS", "LOTE 08 EPI", "LOTE 08 FERRAMENTAS","BOTA FORA EPI", "BOTA FORA FERRAMENTAS", "GERAL FERRAMENTAS", "GERAL EPI","GENEBRA EPI", "GENEBRA FERRAMENTAS"]
+        ["LOTE 01 EPI", "LOTE 01 FERRAMENTAS", "LOTE 03 EPI", "LOTE 03 FERRAMENTAS", "LOTE 05 EPI", "LOTE 05 FERRAMENTAS", "LOTE 08 EPI", "LOTE 08 FERRAMENTAS", "BOTA FORA EPI", "BOTA FORA FERRAMENTAS", "GERAL FERRAMENTAS", "GERAL EPI", "GENEBRA EPI", "GENEBRA FERRAMENTAS"]
     )
 
     # Estado da página para rastrear se o botão de download foi clicado
@@ -94,33 +120,29 @@ def main():
             progress_bar = st.progress(0)
             progress_text = st.empty()
             
-            renamed_files = rename_files_and_get_names(temp_dir, progress_bar, progress_text)
+            renamed_files, arquivos_por_ano_e_categoria = rename_files_and_get_names(temp_dir, progress_bar, progress_text)
             
             # Criar um arquivo zip com os arquivos renomeados
             zip_file_name = output_zip_name + ".zip" if output_zip_name else "Processed_Files.zip"
             with tempfile.NamedTemporaryFile(delete=False) as temp_file:
                 with zipfile.ZipFile(os.path.join(temp_dir, zip_file_name), "w") as zip_file:
-                    total_files = len(renamed_files)
-                    for i, (original_name, new_name) in enumerate(renamed_files.items()):
-                        png_path = os.path.join(temp_dir, new_name)
-                        
-                        if convert_to_pdf:
-                            pdf_path = png_path.replace(".png", ".pdf")
-                            convert_png_to_pdf(png_path, pdf_path)
-                            zip_file.write(pdf_path, arcname=os.path.basename(pdf_path))
-                            os.remove(png_path)
-                        else:
-                            zip_file.write(png_path, arcname=new_name)
-                        
-                        # Atualizar barra de progresso
-                        progress_bar.progress((i + 1) / total_files)
-                        progress_text.text(f"Zipping file {i + 1} of {total_files}")
+                    for ano, categorias in arquivos_por_ano_e_categoria.items():
+                        for categoria, arquivos in categorias.items():
+                            for arquivo in arquivos:
+                                caminho_arquivo = os.path.join(temp_dir, arquivo)
+                                if convert_to_pdf:
+                                    pdf_path = caminho_arquivo.replace(".png", ".pdf")
+                                    convert_png_to_pdf(caminho_arquivo, pdf_path)
+                                    zip_file.write(pdf_path, arcname=os.path.join(str(ano), categoria, os.path.basename(pdf_path)))
+                                    os.remove(caminho_arquivo)
+                                else:
+                                    zip_file.write(caminho_arquivo, arcname=os.path.join(str(ano), categoria, arquivo))
 
                 st.session_state['download_clicked'] = True
                 st.download_button(
-                    label="Download Files", 
-                    data=open(os.path.join(temp_dir, zip_file_name), "rb").read(), 
-                    file_name=zip_file_name, 
+                    label="Download Files",
+                    data=open(os.path.join(temp_dir, zip_file_name), "rb").read(),
+                    file_name=zip_file_name,
                     mime="application/zip"
                 )
 
